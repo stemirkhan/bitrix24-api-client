9a9cd6d31a92645a2d2f9bb6f08f5fc4c6fe75a5
stemirkhan <temirkhan.samurkhanov@mail.ru>
2025-04-06 10:51:25 +0300

    Export Bitrix24 custom exceptions in package __init__.py

--- src/bitrix24_client/__init__.py
+++ src/bitrix24_client/__init__.py
@@ -1,5 +1,23 @@
-# my_package/__init__.py
 from .sync_client import Bitrix24Client
 from .async_client import AsyncBitrix24Client
+from .exceptions import (
+    Bitrix24Error,
+    Bitrix24ConnectionError,
+    Bitrix24TimeoutError,
+    Bitrix24HTTPError,
+    Bitrix24APIError,
+    Bitrix24InvalidResponseError,
+    Bitrix24InvalidBaseURLError,
+)
 
-__all__ = ["Bitrix24Client", "AsyncBitrix24Client"]
+__all__ = [
+    "Bitrix24Client",
+    "AsyncBitrix24Client",
+    "Bitrix24Error",
+    "Bitrix24ConnectionError",
+    "Bitrix24TimeoutError",
+    "Bitrix24HTTPError",
+    "Bitrix24APIError",
+    "Bitrix24InvalidResponseError",
+    "Bitrix24InvalidBaseURLError",
+]
