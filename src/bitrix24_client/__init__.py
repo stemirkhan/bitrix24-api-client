6e20a91d52e18de754c78a3a8a567e05bc1d1697
stemirkhan <temirkhan.samurkhanov@mail.ru>
2025-05-11 13:36:56 +0300

    Added a function for dividing into vats to execute a batch request

--- src/bitrix24_client/__init__.py
+++ src/bitrix24_client/__init__.py
@@ -1,5 +1,6 @@
 from .sync_client import Bitrix24Client
 from .async_client import AsyncBitrix24Client
+from .utils import chunk_batch_requests
 from .exceptions import (
     Bitrix24Error,
     Bitrix24ConnectionError,
@@ -20,4 +21,5 @@ __all__ = [
     "Bitrix24APIError",
     "Bitrix24InvalidResponseError",
     "Bitrix24InvalidBaseURLError",
+    "chunk_batch_requests"
 ]
--- src/bitrix24_client/utils.py
+++ src/bitrix24_client/utils.py
@@ -1,4 +1,6 @@
 from urllib.parse import urlparse
+from typing import List, Dict, Any
+
 
 def is_valid_url(url: str) -> bool:
     """
@@ -12,3 +14,11 @@ def is_valid_url(url: str) -> bool:
     """
     parsed = urlparse(url)
     return parsed.scheme in ("http", "https") and bool(parsed.netloc)
+
+
+def chunk_batch_requests(commands: Dict[str, Any], chunk_size: int = 50) -> List[Dict[str, Any]]:
+    chunked = [
+        {k: commands[k] for k in list(commands.keys())[i:i + chunk_size]}
+        for i in range(0, len(commands), chunk_size)
+    ]
+    return chunked
