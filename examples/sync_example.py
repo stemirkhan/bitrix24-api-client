ffa6fe7b5b75c5f49084828e13fe9864ecf48bcf
stemirkhan <temirkhan.samurkhanov@mail.ru>
2025-05-11 20:00:06 +0300

    updated the usage examples

--- examples/sync_example.py
+++ examples/sync_example.py
@@ -1,15 +1,15 @@
 from src.bitrix24_client.sync_client import Bitrix24Client
 
-client = Bitrix24Client(base_url='https://yourdomain.bitrix24.ru',
-                        access_token='your_webhook_key',
-                        user_id=1) # Optional user_id parameter
+client = Bitrix24Client(base_url='https://example.bitrix24.ru',
+                        access_token='token',
+                        user_id=1)  # Optional user_id parameter
 
-try:
-    result = client.call_method(
-        method='crm.lead.list',
-        params={'select': ['ID', 'TITLE']},
-        fetch_all=True
-    )
-    print("Leads:", result)
-except Exception as e:
-    print(f"Error occurred: {e}")
+client.open_session()
+
+result = client.call_method(
+    method='crm.lead.list',
+    params={'select': ['ID', 'TITLE']},
+    fetch_all=True
+)
+
+client.close_session()
